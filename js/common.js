ee301d58c13e02db88160cf33225c623594c7b3f
Igor <ingarzharov@yandex.ru>
2022-07-11 16:52:38 +0500

    add hamburger menu

--- js/common.js
+++ js/common.js
@@ -1,1 +1,11 @@
-// $('body').hide()
+document.addEventListener('DOMContentLoaded', () => {
+
+	const hamburger = document.querySelector('.header__hamburger');
+
+	hamburger.addEventListener('click', (e) => {
+
+		hamburger.classList.toggle('header__hamburger--active');
+
+	})
+
+})
