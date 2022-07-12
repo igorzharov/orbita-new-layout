43a9639a82c69b5d0c7d208b61bd111c99570567
Igor <ingarzharov@yandex.ru>
2022-07-12 11:28:56 +0500

    Ивент на кнопке каталога

--- js/common.js
+++ js/common.js
@@ -2,9 +2,20 @@ document.addEventListener('DOMContentLoaded', () => {
 
 	const hamburger = document.querySelector('.header__hamburger');
 
-	hamburger.addEventListener('click', (e) => {
+	hamburger.addEventListener('click', () => {
 
 		hamburger.classList.toggle('header__hamburger--active');
+
+	})
+
+	const header__catalog__button = document.querySelector('.header__catalog-button');
+
+	const catalog = document.querySelector('.catalog');
+
+	header__catalog__button.addEventListener('click', () => {
+
+		header__catalog__button.classList.toggle('header__catalog-button--active');
+		catalog.classList.toggle('catalog--active');
 
 	})
 
