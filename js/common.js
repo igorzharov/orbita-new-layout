020e1013c9836122d640b7de1a5096570336b42b
ingvarzharov <ingvarzharov@yandex.ru>
2022-07-15 09:20:49 +0500

    Обновлена логика открытия-закрытия каталога

--- js/common.js
+++ js/common.js
@@ -2,30 +2,49 @@ document.addEventListener('DOMContentLoaded', () => {
 
 	const hamburger = document.querySelector('.header__hamburger');
 
+	const catalog = document.querySelector('.catalog');
+
 	hamburger.addEventListener('click', () => {
 
 		hamburger.classList.toggle('header__hamburger--active');
+		catalog.classList.toggle('catalog--active');
 
 	})
 
 	const headerCatalogButton = document.querySelector('.header__catalog-button');
-
-	const catalog = document.querySelector('.catalog');
 
 	headerCatalogButton.addEventListener('click', () => {
 
 		headerCatalogButton.classList.toggle('header__catalog-button--active');
 		catalog.classList.toggle('catalog--active');
 
+		if (window.matchMedia("(min-width: 860px)").matches) {
+
+			const categories = document.querySelectorAll('.catalog__item');
+
+			categories[0].classList.add('catalog__item--active');
+
+			const childCategories = document.querySelectorAll('.catalog-category');
+
+			childCategories[0].classList.add('catalog-category--active');
+
+		}
+
 	})
 
 	document.addEventListener('click', function (event) {
 
+		if (!event.target.closest('.catalog__item')) return;
+
 		const categories = document.querySelectorAll('.catalog__item');
 
-		// categories.forEach((item) => {
-		// 	item.classList.remove('catalog__item--active');
-		// })
+		if (window.matchMedia("(min-width: 860px)").matches) {
+
+			categories.forEach((item) => {
+				item.classList.remove('catalog__item--active');
+			});
+
+		}
 
 		const category = event.target.closest('.catalog__item');
 
@@ -35,14 +54,18 @@ document.addEventListener('DOMContentLoaded', () => {
 
 		const childCategories = document.querySelectorAll('.catalog-category');
 
-		// childCategories.forEach((item) => {
-		// 	item.classList.remove('catalog-category--active');
-		// })
-		
-		const catalogCategory = document.querySelector(`[parent-category-id="${id}"]`)
+		if (window.matchMedia("(min-width: 860px)").matches) {
+
+			childCategories.forEach((item) => {
+				item.classList.remove('catalog-category--active');
+			});
+
+		}
+
+		const catalogCategory = document.querySelector(`[parent-category-id="${id}"]`);
 
 		catalogCategory.classList.toggle('catalog-category--active');
-	
+
 	});
 
 })
